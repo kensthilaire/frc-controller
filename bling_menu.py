f792b7c72b82262d87b393f54363a73bd23ae2e2
Ken St.Hilaire <kensthilaire@gmail.com>
2023-08-06 17:51:23 -0400

    Fixed indentation issue with the bling_menu application

--- bling_menu.py
+++ bling_menu.py
@@ -34,32 +34,33 @@ def bling_menu( bling ):
 
     print( '\nDone!' )
 
-# command line options handling
-parser = OptionParser()
+if __name__ == '__main__':
+    # command line options handling
+    parser = OptionParser()
 
-parser.add_option(
-    "-l","--leds",dest="leds", default='48',
-    help='Total number of LEDs in the strip')
-parser.add_option(
-    "-s","--segments",dest="segments", default=None,
-    help='Number of LED segments in the strip')
-parser.add_option(
-    "-b","--brightness",dest="brightness", default='255',
-    help='Overall brightness of the LEDs from 0-255, with 255 being the brightest')
+    parser.add_option(
+        "-l","--leds",dest="leds", default='48',
+        help='Total number of LEDs in the strip')
+    parser.add_option(
+        "-s","--segments",dest="segments", default=None,
+        help='Number of LED segments in the strip')
+    parser.add_option(
+        "-b","--brightness",dest="brightness", default='255',
+        help='Overall brightness of the LEDs from 0-255, with 255 being the brightest')
 
 
-# Parse the command line arguments
-(options,args) = parser.parse_args()
+    # Parse the command line arguments
+    (options,args) = parser.parse_args()
 
-if options.segments is not None:
-    num_segments = int(options.segments)
-else:
-    num_segments = None
+    if options.segments is not None:
+        num_segments = int(options.segments)
+    else:
+        num_segments = None
 
-try:
-    bling = Bling(int(options.leds), num_segments, int(options.brightness))
-except ValueError:
-    sys.exit('ERROR: Invalid Brightness Level: %s, must be 0-255' % options.brightness)
+    try:
+        bling = Bling(int(options.leds), num_segments, int(options.brightness))
+    except ValueError:
+        sys.exit('ERROR: Invalid Brightness Level: %s, must be 0-255' % options.brightness)
 
-bling_menu( bling )
+    bling_menu( bling )
 
